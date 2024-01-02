7c64a82c4ccb0701e17432c5e8f360a7648b6009
Kevin D'Ornellas <dornellaskj@gmail.com>
2024-01-02 08:37:22 -0700

    variables

---
+++ VBFundamentals/VBFundamentals/MainModule.vb
@@ -0,0 +1,22 @@
+﻿Module MainModule
+    Dim ProductId As Integer = 1
+    Dim isActive As Boolean = True
+    Dim Name As String = "10 Speed Bike"
+    Dim ListPrice As Decimal = 999.99D
+    Dim SellStartDate As DateTime = #1/1/2019#
+    Dim newObject As Object = "anythng can go here but this datatype is heavy"
+
+    Sub Main()
+        IncrementListPrice()
+        IncrementListPrice()
+        IncrementListPrice()
+        Console.ReadKey()
+    End Sub
+
+    Sub IncrementListPrice()
+        Static ListPrice As Decimal = 0
+        ListPrice = ListPrice + 1
+        Console.WriteLine(ListPrice)
+    End Sub
+
+End Module
---
+++ VBFundamentals/VBFundamentals/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBFundamentals")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VBFundamentals")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ee153447-70b3-4430-aa96-c83742383e50")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
