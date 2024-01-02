6d8f13e3d852b6c3e0f157566167f326d9552fa9
Kevin D'Ornellas <dornellaskj@gmail.com>
2024-01-02 10:39:20 -0700

    adding class and properties

--- VBFundamentals/VBFundamentals/MainModule.vb
+++ VBFundamentals/VBFundamentals/MainModule.vb
@@ -1,16 +1,24 @@
-﻿Module MainModule
+﻿Imports System.Runtime.CompilerServices
+
+Module MainModule
     Dim ProductId As Integer = 1
     Dim isActive As Boolean = True
     Dim Name As String = "10 Speed Bike"
     Dim ListPrice As Decimal = 999.99D
     Dim SellStartDate As DateTime = #1/1/2019#
     Dim newObject As Object = "anythng can go here but this datatype is heavy"
+    Private Const DEFAULT_LIST_PRICE As Decimal = 999.99D
 
     Sub Main()
         IncrementListPrice()
         IncrementListPrice()
         IncrementListPrice()
+        Dim prod As New Product()
+        prod.SellStartDate = Date.Now()
+        prod.CalculateSellEndDate(5)
+        Console.WriteLine(prod.SellEndDate)
         Console.ReadKey()
+
     End Sub
 
     Sub IncrementListPrice()
---
+++ VBFundamentals/VBFundamentals/Product.vb
@@ -0,0 +1,11 @@
+﻿Public Class Product
+    Public Property IsActive() As Boolean
+    Public Property Name() As String
+    Public Property ProductNumber() As String
+    Public Property SellStartDate As DateTime
+    Public Property SellEndDate As DateTime
+
+    Sub CalculateSellEndDate(ByVal days As Integer)
+        SellEndDate = SellStartDate.AddDays(days)
+    End Sub
+End Class
